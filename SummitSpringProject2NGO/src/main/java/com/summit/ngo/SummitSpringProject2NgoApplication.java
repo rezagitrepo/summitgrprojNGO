98def252ad0b8b5224e24646fe93a70526fcd083
rezaalicse <reza@DESKTOP-QBNMNNQ>
2021-05-12 16:02:37 -0500

    this is first commit

---
+++ SummitSpringProject2NGO/src/main/java/com/summit/ngo/SummitSpringProject2NgoApplication.java
@@ -0,0 +1,13 @@
+package com.summit.ngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummitSpringProject2NgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummitSpringProject2NgoApplication.class, args);
+	}
+
+}
---
+++ SummitSpringProject2NGO/src/test/java/com/summit/ngo/SummitSpringProject2NgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.summit.ngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummitSpringProject2NgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
