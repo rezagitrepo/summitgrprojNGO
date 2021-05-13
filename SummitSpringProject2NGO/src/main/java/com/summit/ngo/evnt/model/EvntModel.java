f71417e6f7d480b0e2a52ab89a1415a50ce7ae69
rezaalicse <reza@DESKTOP-QBNMNNQ>
2021-05-13 11:23:30 -0500

    commit after evnt and registration model

---
+++ SummitSpringProject2NGO/src/main/java/com/summit/ngo/evnt/model/EvntModel.java
@@ -0,0 +1,126 @@
+package com.summit.ngo.evnt.model;
+
+import java.sql.Timestamp;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.summit.ngo.registration.model.RegistrationModel;
+
+
+@Entity
+@Table
+public class EvntModel {
+	
+	@Id
+	@GeneratedValue
+	Integer id;
+	String name;
+	String description;
+	String category;
+	String location;
+	Timestamp start_date;
+	Timestamp end_date;
+	@OneToMany(mappedBy = "evntmodel",cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private Set<RegistrationModel> registrationModel;
+	
+	public EvntModel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public EvntModel(Integer id, String name, String description, String category, String location,
+			Timestamp start_date, Timestamp end_date, Set<RegistrationModel> registrationModel) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.category = category;
+		this.location = location;
+		this.start_date = start_date;
+		this.end_date = end_date;
+		this.registrationModel = registrationModel;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public Timestamp getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(Timestamp start_date) {
+		this.start_date = start_date;
+	}
+
+	public Timestamp getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(Timestamp end_date) {
+		this.end_date = end_date;
+	}
+
+	public Set<RegistrationModel> getRegistrationModel() {
+		return registrationModel;
+	}
+
+	public void setRegistrationModel(Set<RegistrationModel> registrationModel) {
+		this.registrationModel = registrationModel;
+	}
+
+	@Override
+	public String toString() {
+		return "EvntModel [id=" + id + ", name=" + name + ", description=" + description + ", category=" + category
+				+ ", location=" + location + ", start_date=" + start_date + ", end_date=" + end_date
+				+ ", registrationModel=" + registrationModel + "]";
+	}
+
+	
+	
+	
+	
+}
