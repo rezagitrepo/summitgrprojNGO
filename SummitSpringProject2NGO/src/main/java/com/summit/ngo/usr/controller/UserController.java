3ef08658730f4af8c7176f1b3e26f352b477b6f9
Ken Lee <Ken Lee@host.docker.internal>
2021-05-17 19:46:56 -0700

    view files with user controller,repo's logic

--- SummitSpringProject2NGO/src/main/java/com/summit/ngo/usr/controller/UserController.java
+++ SummitSpringProject2NGO/src/main/java/com/summit/ngo/usr/controller/UserController.java
@@ -1,118 +1,155 @@
 package com.summit.ngo.usr.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.summit.ngo.usr.model.User;
 import com.summit.ngo.usr.repository.RoleRepository;
 import com.summit.ngo.usr.repository.UserRepository;
+import com.summit.ngo.usr.service.UserService;
+
+import antlr.collections.List;
 
 @Controller
 public class UserController {
 
 	@Autowired
-	UserRepository userRepo;
+	UserService userService;
 	@Autowired
 	RoleRepository roleRepo;
+	@Autowired
+	UserRepository userRepo;
 	
 	@Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-	 
+	private BCryptPasswordEncoder passwordEncoder;
+	
 	@GetMapping("/")
-	public String root() {
-	     
-		System.out.println("Here it is index controller");
-		
-		//@RequestParam("principal.username")String email
-//		 User user = userRepo.findByEmail(email);
-//	        String roleName = roleRepo.findRole(user.getId());
-//	        
-//	        Boolean password = passwordEncoder.matches(email, user.getPassword());
-//	        
-//	        if(roleName.equals("ROLE_ADMIN") && password) {System.out.println("I am Admin User");return "redirect:/hello";}
-//	        else {System.out.println("I am General User"); return "index";}
-		 
-		 return "index";
-	    }
+	public String index() {
+		System.out.println("index executed");
+		return "/login";
+	}
+	
+	@GetMapping("/login")
+	public String login(Model model, String error, String logout) {
 
-//	    @GetMapping("/login")
-//	    public String login(Model model) {
-//	        return "login";
-//	    }
-	   
-	 
-	//@PostMapping("/login")
-	  
-	 //@RequestMapping(value = "/login", method = RequestMethod.GET)
-	 @GetMapping("/login") 
-	 public String login(Model model,String error, String logout) {
-	        //public String login(Model model, String error, String logout)
-	    	
-	    	
-	    	if (error != null)
-	            model.addAttribute("errorMsg", "Your username and password are invalid.");
+		if (error != null)
+			model.addAttribute("errorMsg", "Your username and password are invalid.");
 
-	        if (logout != null)
-	            model.addAttribute("msg", "You have been logged out successfully.");
-	     System.out.println("LogIn page Executed");   
-	     return "login";
-	       
-	        
-	        //model.addAttribute("msg", "Value of email is "+email);
-	        
-	     
-	     
-	     
-	    }
-	    
+		if (logout != null)
+			model.addAttribute("msg", "You have been logged out successfully.");
+		System.out.println("LogIn page Executed");
+		return "/login";
+	}
+	
+	@PostMapping("/checkUser")
+	public String checkUser(Model model, @RequestParam("username") String email, @RequestParam("password") String pass) {
 
-	    
-		@PostMapping("/hello")
-		public String hello(Model model, @RequestParam("username") String email, @RequestParam("password") String pass) {
+		model.addAttribute("msg", "Hi." + email + "!");
+		System.out.println("checkUser is Executed");
 
-			model.addAttribute("msg", "Hi." + email + "!");
-			// model.addAttribute("lists",lists);
-			System.out.println("hello Controller is Executed");
-			System.out.println("UserName(webpage): " + email);
+		User user = userService.findByEmail(email);
+		String roleName = roleRepo.findRole(user.getId());
 
-			User user = userRepo.findByEmail(email);
-			String roleName = roleRepo.findRole(user.getId());
-			
-			System.out.println("this is from database role name:"+roleName);
-			
-			Boolean password = passwordEncoder.matches(pass, user.getPassword());
 
-			System.out.println("Value of Boolean Password: "+password);
-			
-			if (roleName.equals("ROLE_ADMIN") && password) {
-				System.out.println("I am Admin User");
-				return "hello";
-			} else {
-				if (roleName.equals("ROLE_USER") && password) {
-					System.out.println("I am General User");
-					return "redirect:/hello1";
-				} else
-					return "login";
+		Boolean password = passwordEncoder.matches(pass, user.getPassword());
 
-			}
 
-			// return "hello";
+		if (roleName.equals("ROLE_ADMIN") && password) {
+			System.out.println("I am Admin User");
+			return "redirect:/user";
+		} else {
+			if (roleName.equals("ROLE_USER") && password) {
+				System.out.println("I am General User");
+				return "redirect:/view";
+			} else
+				return "/login";
 		}
-	    
-	    @GetMapping("/hello1")
-	    public String hello1(Model model) {
-	        //@RequestParam("username")String id
-	    	model.addAttribute("msg","Hi.");
-	    	//model.addAttribute("lists",lists);
-	    	System.out.println("Hello is Executed");
-	    	//System.out.println("ID:"+id);
-	    	return "hello1";
-	    }
-	    
-
+	}
+	
+	@GetMapping("/generalUser")
+	public String generalUser(Model model) {
+		model.addAttribute("msg", "Hi.");
+		System.out.println("generalUser is Executed");
+		return "/generalUser";
+	}
+	
+	@GetMapping("/user")
+	public ModelAndView user(Model model) {
+		System.out.println("index executed");
+		List<User> lists = userRepo.userAndRole();
+		String first_name = "";
+		String last_name = "";
+		String user = "";
+		
+		for(int i=0; i<lists.size(); i++) {
+			first_name = lists.get(i).getFirst_name();
+			last_name = lists.get(i).getLast_name();
+			
+			System.out.println(lists);
+			// role validation
+			if(lists.equals("ROLE_ADMIN")) {
+				user = "admin";
+			}else {
+				user = "user";
+			}
+		}
+		
+		// first name + last name
+		String name = first_name+" "+last_name;
+		
+		
+		model.addAttribute("name",name);
+		model.addAttribute("lists",lists);
+		model.addAttribute("user",user);
+		return new ModelAndView("/user");
+	}
+	
+	@GetMapping("/edit_popup/{id}")
+	public ModelAndView editUser(@PathVariable(name="id") int id) {
+		System.out.println("edit executes");
+		ModelAndView mav = new ModelAndView("edit_popup");
+		User user = userService.findById(id);
+		mav.addObject("user",user);
+		return mav;
+	} 
+	
+	@PostMapping
+	public String saveUser(@Valid User user, Model model) {
+		userService.save(user);
+		model.addAttribute("user",user);
+		return "redirect:/user";
+	}
+	
+	@GetMapping("/delete_popup/{id}")
+	public String delete(@PathVariable(name="id") int id) {
+		System.out.println("delete executes");
+		userService.deleteById(id);
+		return "redirect:/user";
+	}
+	
+	@GetMapping("/new")
+	public String newUser() {
+		return "/newUser_popup";
+	}
+	
+	@GetMapping("/event")
+	public ModelAndView event(Model model) {
+		System.out.println("event executed");
+		return new ModelAndView("/event");
+	}
+	
+	@GetMapping("/view")
+	public ModelAndView view(Model model) {
+		System.out.println("event executed");
+		return new ModelAndView("/view");
+	}
 }
